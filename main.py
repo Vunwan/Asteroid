81501c175a1789d7f6d0d3ada6bb92feb8a37fa5
Vunwan <vunwan@hotmail.fr>
2026-04-28 08:28:52 +0200

    Added movement to player

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
-        player.update(screen)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
